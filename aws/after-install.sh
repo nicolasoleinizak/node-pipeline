c597a43da1d35d4ad119209aca47dda0219af228
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2023-03-08 11:43:27 -0300

    adjust pipeline

--- aws/after-install.sh
+++ aws/after-install.sh
@@ -1,1 +1,14 @@
-aws s3 cp s3://pruebas-microservicios-webappdeploymentbucket-kiyqmiamkemq/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+# This script is executed after the application is installed on the EC2 instance
+
+# Copy the app.tar.gz file from the S3 bucket to the EC2 instance
+aws s3 cp s3://pruebas-microservicios-webappdeploymentbucket-kiyqmiamkemq/app.tar.gz /home/ec2-user/nodeserver
+
+# Navigate to the directory where the app.tar.gz file is located
+cd /home/ec2-user/nodeserver
+
+# Extract the contents of the app.tar.gz file
+tar -xvf app.tar.gz
+
+# Remove the app.tar.gz file
+rm -rf app.tar.gz
+
---
+++ aws/application-start.sh
@@ -0,0 +1,6 @@
+# Description: This script is used to start the node server
+
+# Navigate to the directory where the app.tar.gz file is located
+cd /home/ec2-user/nodeserver/
+
+npm run start

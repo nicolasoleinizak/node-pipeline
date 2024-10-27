86c332065bd3a200855f3fbc7e49acfec157c427
Nicolas Oleinizak <nicolas.oleinizak@gmail.com>
2024-10-26 23:00:55 -0300

    feat: pipeline

---
+++ aws/after-install.sh
@@ -0,0 +1,1 @@
+aws s3 cp s3://pruebas-microservicios-webappdeploymentbucket-kiyqmiamkemq/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
